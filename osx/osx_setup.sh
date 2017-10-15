52cda10f99ff459d5fc7c25d33c94c0d46c0f028
Alcides Fonseca <me@alcidesfonseca.com>
2017-10-16 00:30:26 +0100

    Updated homebrew

--- osx/osx_setup.sh
+++ osx/osx_setup.sh
@@ -6,6 +6,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+echo "Hidden windows blurs mac icon"
+defaults write com.apple.Dock showhidden -bool TRUE; killall Dock
+
 echo "Menu bar: disable transparency"
 defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
 
