5f4ad7ff8c79d2110e78eba28e1f7b65f34bcdaf
Alcides Fonseca <me@alcidesfonseca.com>
2020-03-06 11:58:52 +0000

    Added script to configure DI printers

--- dotfiles/config.sh
+++ dotfiles/config.sh
@@ -1,2 +1,2 @@
-lpadmin -p "SecretariaPB" -D "Secretaria (P&B)" -L "C6.3" -E -v smb://printserver.fc.ul.pt/Canon_IN -m drv:///sample.drv/generpcl.ppd -o Duplex=DuplexNoTumble
-lpadmin -p "SecretariaCores" -D "Secretaria (Cores)" -L "C6.3" -E -v smb://printserver.fc.ul.pt/Canon_IN -m drv:///sample.drv/generic.ppd -o Duplex=DuplexNoTumble
+lpadmin -p "SecretariaPB" -D "Secretaria (P&B)" -L "C6.3" -E -v smb://printserver.fc.ul.pt/Canon_IN -m drv:///sample.drv/generpcl.ppd -o Duplex=DuplexTumble -o Option1=True
+lpadmin -p "SecretariaCores" -D "Secretaria (Cores)" -L "C6.3" -E -v smb://printserver.fc.ul.pt/Canon_IN -m drv:///sample.drv/generic.ppd -o Duplex=DuplexTumble -o Option1=True
